3928d5d836b52beafae358c8d4ba1c4158b8beea
hollyjphilly <61254925+hollyjphilly@users.noreply.github.com>
2021-01-25 15:21:02 -0500

    Created Array#hash and String#has

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -4,11 +4,18 @@ end
 
 class Array
   def hash
+
+    b_arr = self.map.with_index { |e, i| (e * 3**i).to_s(2).to_i }
+    b_sum = b_arr.inject { |acc, e| acc ^ e }.to_s
+    b_sum.to_i(2)
   end
 end
 
 class String
   def hash
+    alpha = ('a'..'z').to_a + ('A'..'Z').to_a 
+    arr = self.chars.map { |c| alpha.index(c) }
+    arr.hash
   end
 end
 
