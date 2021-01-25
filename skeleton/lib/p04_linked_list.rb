587f46421235f5e6c37a922a44227f208975a637
Ray <raysidarta@gmail.com>
2021-01-25 17:41:28 -0500

    Finish LinkedList

--- skeleton/lib/p04_linked_list.rb
+++ skeleton/lib/p04_linked_list.rb
@@ -1,3 +1,5 @@
+
+
 class Node
   attr_reader :key
   attr_accessor :val, :next, :prev
@@ -20,6 +22,8 @@ class Node
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
     @head = Node.new()
     @tail = Node.new()
@@ -45,12 +49,8 @@ class LinkedList
   end
 
   def get(key)
-    a_node = @head
-    until key == a_node.key
-      return nil if a_node == @tail
-      a_node = a_node.next
-    end
-    a_node.val
+    a_node = get_node(key) 
+    a_node ?  a_node.val : nil
   end
 
   def include?(key)
@@ -67,16 +67,40 @@ class LinkedList
   end
 
   def update(key, val)
+    a_node = get_node(key) 
+    a_node ?  a_node.val = val : nil
   end
 
   def remove(key)
+    a_node = get_node(key)
+    before = a_node.prev
+    after = a_node.next
+    before.next = after
+    after.prev = before
   end
 
-  def each
+  def each(&prc)
+
+    a_node = first
+    until  a_node == @tail
+      prc.call(a_node)
+      a_node = a_node.next
+    end
+
   end
+
+  def get_node(key)
+     a_node = @head
+    until key == a_node.key
+      return nil if a_node == @tail
+      a_node = a_node.next
+    end
+    a_node
+  end
+
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
