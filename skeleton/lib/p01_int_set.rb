4a2e880e1f543699b98f6338593f4d646a885d81
Ray <raysidarta@gmail.com>
2021-01-25 13:40:52 -0500

    Finish MaxIntSet

--- skeleton/lib/p01_int_set.rb
+++ skeleton/lib/p01_int_set.rb
@@ -1,17 +1,34 @@
 class MaxIntSet
-  def initialize(max)
+
+  attr_reader :store, :max
+
+
+  def initialize(m)
+    @store = Array.new(m, false)
+    @max = m
+
   end
 
   def insert(num)
+    raise 'Out of bounds' unless num.between?(0,max) 
+    store[num] = true
   end
 
   def remove(num)
+    raise 'Out of bounds' unless num.between?(0,max) 
+    store[num] = false
+
+
   end
 
   def include?(num)
+    raise 'Out of bounds' unless num.between?(0,max) 
+    store[num] == true
   end
 
   private
+
+  
 
   def is_valid?(num)
   end
@@ -27,6 +44,7 @@ class IntSet
   end
 
   def insert(num)
+    self[num]<< num 
   end
 
   def remove(num)
@@ -39,6 +57,7 @@ class IntSet
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num%num_buckets]
   end
 
   def num_buckets
